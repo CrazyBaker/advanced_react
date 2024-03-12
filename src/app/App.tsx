f019550ce67a86200d7cbe8e5651682bf13da0c5
a.zigunov <a.zigunov@start.ru>
2024-03-12 20:40:24 +0300

    rebuild for future sliced design

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,10 @@
 import { Routes, Route, Link } from 'react-router-dom'
-import { Suspense, useContext } from 'react'
+import { Suspense } from 'react'
 
-import  { AboutPageAsync } from './pages/AboutPage/AboutPage.async'
-import { MainPageAsync } from './pages/MainPage/MainPage.async'
-import { classNames } from './helpers/classNames/classNames'
-import Counter from './components/Counter'
-import { useTheme } from './theme/useTheme'
+import { classNames } from 'shared/lib/classNames/classNames'
+import { useTheme } from 'app/providers/ThemeProvider'
+import { AboutPage } from 'pages/AboutPage'
+import { MainPage } from 'pages/MainPage'
 import './styles/index.scss'
 
 
@@ -19,11 +18,10 @@ export default function App() {
             <Link to='/about'>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path='/about' element={<AboutPageAsync />}/>
-                    <Route path='/' element={<MainPageAsync />}/>
+                    <Route path='/about' element={<AboutPage />}/>
+                    <Route path='/' element={<MainPage />}/>
                 </Routes>
             </Suspense>
-            <Counter />
         </div>
     )
 }
