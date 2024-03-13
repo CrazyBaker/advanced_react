b0f72556ae66538edf48926ad8a14eb305e210a1
a.zigunov <a.zigunov@start.ru>
2024-03-13 12:13:29 +0300

    add navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,22 +1,18 @@
-import { Routes, Route, Link } from 'react-router-dom'
-import { Suspense } from 'react'
-
 import { classNames } from 'shared/lib/classNames/classNames'
 import { useTheme } from 'app/providers/ThemeProvider'
 import { AppRouter } from 'app/providers/router'
+import { Navbar } from 'widgets/Navbar'
 
 import './styles/index.scss'
-
 
 export default function App() {
     const {theme, toggleTheme} = useTheme()
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <button onClick={toggleTheme}>TOGGLE</button>
-            <Link to='/'>Главная</Link>
-            <Link to='/about'>О сайте</Link>
+            <Navbar />
             <AppRouter />
+            <button onClick={toggleTheme}>TOGGLE</button>
         </div>
     )
 }
---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,24 @@
+import { AppLink, AppLinkTheme } from 'shared/ui/AppLink'
+import { classNames } from 'shared/lib/classNames/classNames'
+
+import styles from './Navbar.module.scss'
+
+interface NavbarProps {
+  className?: string,
+
+}
+
+export function Navbar({className}:NavbarProps) {
+  return (
+    <div className={classNames(styles.navbar, {}, [className])}>
+        <div className={styles.links}>
+            <AppLink theme={AppLinkTheme.SECONDARY} to={'/'} className={styles.mainLink}>
+                Главная
+            </AppLink>
+            <AppLink theme={AppLinkTheme.SECONDARY} to={'/about'}>
+                О сайте
+            </AppLink>
+        </div>
+     </div>
+  )
+}
