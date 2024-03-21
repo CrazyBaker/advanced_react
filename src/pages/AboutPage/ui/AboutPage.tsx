3ea0a38496db106392bb883038e71d592eb10cf3
a.zigunov <a.zigunov@start.ru>
2024-03-21 17:32:11 +0300

    add i18n

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -1,5 +1,9 @@
+import { useTranslation } from "react-i18next"
+
 export default function AboutPage() {
+  const { t } = useTranslation('about')
+
   return (
-    <div>ABOUTE PAGE</div>
+    <div>{ t('О сайте') }</div>
   )
 }
