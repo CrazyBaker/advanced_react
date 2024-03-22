4f27f09da3d3564729971621c1e86289b18bf534
a.zigunov <a.zigunov@start.ru>
2024-03-22 15:26:43 +0300

    add hotmodulereplacement plugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -11,6 +11,7 @@ export default function buildPlugins({paths, isDev}:BuildOptions): webpack.Webpa
             filename: 'css/[name].[contenthash:8].css',
             chunkFilename: 'css/[name].[contenthash:8].css'
         }),
-        new webpack.DefinePlugin({ __IS_DEV__: JSON.stringify(isDev) })
+        new webpack.DefinePlugin({ __IS_DEV__: JSON.stringify(isDev) }),
+        new webpack.HotModuleReplacementPlugin()
     ]
 }
